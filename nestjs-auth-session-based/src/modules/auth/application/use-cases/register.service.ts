54c1e1d36d95894e48ad5f16de3be90e81549ee1
Alessandro Mangone BID <38384237+anotherbuginthecode@users.noreply.github.com>
2025-08-22 11:10:01 +0200

    refactor: use GetUserByEmailService and CreateUserService instead of users repository directly

--- nestjs-auth-session-based/src/modules/auth/application/use-cases/register.service.ts
+++ nestjs-auth-session-based/src/modules/auth/application/use-cases/register.service.ts
@@ -1,21 +1,23 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { USER_REPOSITORY } from '@/modules/users/domain/interfaces/user.interface';
-import { UserRepository } from '@/modules/users/infrastructure/repositories/user.repository';
 import { RegisterDto } from '../dto/register.dto';
 import { Logger } from 'nestjs-pino';
 import { User } from '@/modules/users/domain/entities/user.entity';
 import { UserMapper } from '@/modules/users/infrastructure/mappers/user.mapper';
+import { CreateUserService } from '@/modules/users/application/use-cases/create-user.service';
+import { GetUserByEmailService } from '@/modules/users/application/use-cases/get-user-by-email.service';
 
 @Injectable()
 export class RegisterService {
   constructor(
-    @Inject(USER_REPOSITORY)
-    private readonly userRepository: UserRepository,
+    @Inject(CreateUserService)
+    private readonly createUserService: CreateUserService,
+    @Inject(GetUserByEmailService)
+    private readonly getUserByEmailService: GetUserByEmailService,
     private readonly logger: Logger,
   ) {}
 
   async execute({ email, password }: RegisterDto): Promise<User> {
-    const existingUser = await this.userRepository.findByEmail(email);
+    const existingUser = await this.getUserByEmailService.execute(email);
     if (existingUser) {
       this.logger.warn(
         `Registration failed: User with email ${email} already exists.`,
@@ -25,7 +27,7 @@ export class RegisterService {
 
     const user = new User(crypto.randomUUID(), email, password);
     await user.setPassword();
-    await this.userRepository.create(user);
+    await this.createUserService.execute(user);
     this.logger.log(`User registered successfully: ${user.id}`);
     return UserMapper.toDto(user);
   }
